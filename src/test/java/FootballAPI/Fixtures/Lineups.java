1e149fdd8e4a66e575ee209094e84f3d618ad94a
Raja Junaid Ullah Khan <junaidraja2@gmail.com>
2023-08-08 16:15:22 +0500

    New

--- src/test/java/FootballAPI/Fixtures/Lineups.java
+++ src/test/java/FootballAPI/Fixtures/Lineups.java
@@ -6,10 +6,10 @@ import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItem;
 
-public class Stats {
+public class Lineups {
 
     @Test
-    public void stats(){
+    public void lineup(){
 
         String apiKey = "781dbb368amsh76aa4477e882173p1255a3jsn3757c2d59020"; // RapidAPI key
         String fixture = "37899"; // Team Number
@@ -18,7 +18,7 @@ public class Stats {
         baseURI = "https://api-football-beta.p.rapidapi.com";
         given()
                 .header("X-RapidAPI-Key", apiKey)  // Adding the API key as a header
-                .get("/fixtures/statistics?fixture=" + fixture)
+                .get("/fixtures/lineups?fixture=" + fixture)
                 .then()
                 .statusCode(200)
                 .body("response.team.name" , hasItem("Paris Saint Germain"))
