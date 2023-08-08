da24fe284fed66dc823aad7918aee8f809d06754
Raja Junaid Ullah Khan <junaidraja2@gmail.com>
2023-08-08 17:22:24 +0500

    New

--- src/test/java/FootballAPI/Players/SeasonforPlayers.java
+++ src/test/java/FootballAPI/Players/SeasonforPlayers.java
@@ -4,25 +4,23 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.equalTo;
 
-public class TopScorer {
+public class SeasonforPlayers {
 
     @Test
-    public void Scorer(){
+    public void Season(){
 
         String apiKey = "781dbb368amsh76aa4477e882173p1255a3jsn3757c2d59020"; // RapidAPI key
-        String Season = "2019"; // Season Number
-        String League = "39"; //League Number
 
         // Sending the request and getting the response
         baseURI = "https://api-football-beta.p.rapidapi.com";
         given()
                 .header("X-RapidAPI-Key", apiKey)  // Adding the API key as a header
-                .get("/players/topscorers?season=" + Season + "&league=" + League)
+                .get("/players/seasons")
                 .then()
                 .statusCode(200)
-                .body("response.player.name" , hasItem("J. Vardy"))
+                .body("results" , equalTo(40))
                 .log().all();
 
     }
